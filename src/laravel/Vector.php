822458e8f0c6ec32d2704c9e573a88876f4c9890
Laurent Cochet <Cochetto75@gmail.com>
2023-04-06 18:12:47 +0200

    fix(): Fix error in laravel methods signature not matching interface

--- src/laravel/Vector.php
+++ src/laravel/Vector.php
@@ -16,7 +16,7 @@ class Vector extends \Pgvector\Vector implements Castable
                 // no need for dimensions
             }
 
-            public function get(Model $model, string $key, mixed $value, array $attributes): ?Vector
+            public function get($model, string $key, mixed $value, array $attributes): ?Vector
             {
                 if (is_null($value)) {
                     return null;
@@ -27,7 +27,7 @@ class Vector extends \Pgvector\Vector implements Castable
                 return new Vector($value);
             }
 
-            public function set(Model $model, string $key, mixed $value, array $attributes): ?string
+            public function set($model, string $key, mixed $value, array $attributes): ?string
             {
                 if (is_null($value)) {
                     return null;
